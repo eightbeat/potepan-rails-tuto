a3391a39695b9e53f80ffed6c825d2b3630d66a5
eightbeat <sansei.kid011@gmail.com>
2019-06-25 12:38:07 +0900

    finish-10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,21 +1,22 @@
 module SessionsHelper
+  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  # ユーザーを永続的セッションに記憶する
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # 永続的セッションを破棄する
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
   end
 
+  # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -28,8 +29,16 @@ module SessionsHelper
     end
   end
 
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
+  end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
   # 現在のユーザーをログアウトする
@@ -37,5 +46,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
